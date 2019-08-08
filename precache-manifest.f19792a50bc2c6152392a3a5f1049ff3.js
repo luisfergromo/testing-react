211ef3e0fa72474b8074ddbd2ad5f11791c4a663
Luis <is696372@iteso.mx>
2019-08-08 12:16:11 -0500

    Updates

---
+++ precache-manifest.f19792a50bc2c6152392a3a5f1049ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218565835e6fb815995171479a74bfab",
+    "url": "/testing-react/index.html"
+  },
+  {
+    "revision": "994e5b549639f0d7a2da",
+    "url": "/testing-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testing-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "994e5b549639f0d7a2da",
+    "url": "/testing-react/static/js/main.5466c1fa.chunk.js"
+  },
+  {
+    "revision": "3acb858a2fd2bdb5a07f",
+    "url": "/testing-react/static/js/runtime~main.f58d18e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
